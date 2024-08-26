9402f9bef444dd9ce24a1722ad3539eef8349eaa
cham0287 <crtmt97@gmail.com>
2024-08-26 21:34:50 +0900

    build: 웹팩 공통 config 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,10 @@
 const path = require("path");
+const { merge } = require("webpack-merge");
 
-const esmConfig = {
+const commonConfig = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.mjs",
-    library: {
-      type: "module",
-    },
-    clean: true,
-  },
-  experiments: {
-    outputModule: true, // ESM 출력을 위해 필요합니다.
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader", // 필요한 경우 Babel을 통해 ES6+ 코드를 변환합니다.
-        },
-      },
-    ],
-  },
-  target: "web", // 대상 환경을 설정합니다.
-  mode: "production", // 배포용으로 번들링합니다.
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-};
-
-const cjsConfig = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.cjs.js",
-    library: {
-      type: "commonjs2",
-    },
     clean: true,
   },
   module: {
@@ -52,11 +18,33 @@ const cjsConfig = {
       },
     ],
   },
-  target: "node", // Node.js 환경을 대상으로 합니다.
-  mode: "production",
+  mode: "production", // 배포용으로 번들링합니다.
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
 };
+
+const esmConfig = merge(commonConfig, {
+  output: {
+    filename: "bundle.mjs",
+    library: {
+      type: "module",
+    },
+  },
+  experiments: {
+    outputModule: true, // ESM 출력을 위해 필요합니다.
+  },
+  target: "web", // 대상 환경을 설정합니다.
+});
+
+const cjsConfig = merge(commonConfig, {
+  output: {
+    filename: "bundle.cjs.js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+  target: "node", // Node.js 환경을 대상으로 합니다.
+});
 
 module.exports = [esmConfig, cjsConfig];
